5028ca61682655bb9b061f2c0537396c4aac4273
peksharasanjana <ddmaduwa567@gmail.com>
2025-02-20 07:58:40 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~qIMG0QDD#PAsBXx4GMHsdxY7tcGIiMW4obwvbj7N4oAlIPl7dxb8",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~3RtTSKhJ#SIWHW1OKGLsrok_A0XePHedWqVHqYO1STv1Il9VfYQw",
 PORT: process.env.PORT || "8000"
 };
